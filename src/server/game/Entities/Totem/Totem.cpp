c3092c321639fb03e741fffb5a587a958091c942
ProjectStarGate <sharkipaust@web.de>
2011-12-19 00:18:49 +0100

    Core/Totems: Updated Totem

--- src/server/game/Entities/Totem/Totem.cpp
+++ src/server/game/Entities/Totem/Totem.cpp
@@ -113,13 +113,24 @@ void Totem::InitStats(uint32 duration) {
 	SetLevel(m_owner->getLevel());
 }
 
-void Totem::InitSummon() {
-	if (m_type == TOTEM_PASSIVE)
-		CastSpell(this, GetSpell(), true);
-
-	// Some totems can have both instant effect and passive spell
-	if (GetSpell(1))
-		CastSpell(this, GetSpell(1), true);
+void Totem::InitSummon()
+{
+    if (m_type == TOTEM_PASSIVE)
+    {
+        switch (GetSpell())
+        {
+            case 33663: // Earth Elemental Totem
+            case 32982: // Fire Elemental Totem
+            case 50461: // Anti-Magic Zone
+                CastSpell(this, GetSpell(), true);
+                break;
+            default:
+                AddAura(GetSpell(), this);
+                break;
+        }
+    }
+    if (GetSpell(1))
+        CastSpell(this, GetSpell(1), true);
 }
 
 void Totem::UnSummon() {
