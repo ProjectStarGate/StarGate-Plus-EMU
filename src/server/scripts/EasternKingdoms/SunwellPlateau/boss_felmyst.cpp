8e75d2976ae863557992e69353a23af759346eae
ProjectStarGate <sharkipaust@web.de>
2011-12-21 07:04:07 +0100

    Project Update

--- src/server/scripts/EasternKingdoms/SunwellPlateau/boss_felmyst.cpp
+++ src/server/scripts/EasternKingdoms/SunwellPlateau/boss_felmyst.cpp
@@ -3,7 +3,7 @@
  *
  * Copyright (C) 2008-2011 Trinity <http://www.trinitycore.org/>
  *
- * Copyright (C) 2010-2011 ArkCORE <http://www.arkania.net/>
+ * Copyright (C) 2010-2012 Project-StarGate-Emu
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the
--- src/server/scripts/EasternKingdoms/SunwellPlateau/instance_sunwell_plateau.cpp
+++ src/server/scripts/EasternKingdoms/SunwellPlateau/instance_sunwell_plateau.cpp
@@ -3,7 +3,7 @@
  *
  * Copyright (C) 2008-2011 Trinity <http://www.trinitycore.org/>
  *
- * Copyright (C) 2010-2011 ArkCORE <http://www.arkania.net/>
+ * Copyright (C) 2010-2012 Project-StarGate-Emu
  * Copyright (C) 2006-2009 ScriptDev2 <https://scriptdev2.svn.sourceforge.net/>
  *
  * This program is free software; you can redistribute it and/or modify it
